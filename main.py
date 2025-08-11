673ffa9278652e7e360dfbbceac01509fe448650
graphyke <gueretlouis13@gmail.com>
2025-08-11 10:08:06 +0200

    add late shipment

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from fastapi import FastAPI, Query
 
 shipments = {
     "690003171": {
-    "has_error": False,
-    "error_detail": None,		
-    "status": "completed_delivered",			
-    "latest_shipping_date": "17/07/2025",
-    "earliest_shipping_date": "25/07/2025",
-    "created_at": "05/07/2025",			
-    "shipments": [{
-        "carrier_code": "colissimo",		
-        "tracking_link": "https://www.laposte.fr/outils/suivre-vos-envois?code=9M00832571464",		
-        "created_at": "14/07/2025"		
-    }],
-}
+        "has_error": False,
+        "error_detail": None,		
+        "status": "completed_delivered",			
+        "latest_shipping_date": "17/07/2025",
+        "earliest_shipping_date": "25/07/2025",
+        "created_at": "05/07/2025",			
+        "shipments": [{
+            "carrier_code": "colissimo",		
+            "tracking_link": "https://www.laposte.fr/outils/suivre-vos-envois?code=9M00832571464",		
+            "created_at": "14/07/2025"		
+        }],
+    },
+    "100000000": {
+        "has_error": False,
+        "error_detail": None,		
+        "status": "complete_late",			
+        "latest_shipping_date": "17/08/2025",
+        "earliest_shipping_date": "25/08/2025",
+        "created_at": "05/07/2025",			
+        "shipments": [],
+    }
 }
 
 
